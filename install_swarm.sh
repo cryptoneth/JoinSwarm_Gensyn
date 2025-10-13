cc085ac0f074fdefb7ef9ceea02b4fb41ad29f17
CryptonETH <149672650+cryptoneth@users.noreply.github.com>
2025-10-13 15:25:50 +0330

    Update install_swarm.sh

--- install_swarm.sh
+++ install_swarm.sh
@@ -39,8 +39,16 @@ source ~/.bashrc
 
 # Install Docker before creating screen
 echo "Installing Docker..."
-sudo apt update -y && sudo apt upgrade -y
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg lsb-release -y
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo systemctl start docker
+sudo systemctl enable docker
 
 # Step 2: Clone Repository
 echo "Cloning repository..."
